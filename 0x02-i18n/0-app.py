5494bab290583b3f2e854bc237f60f6ac3630a6f
reckafella <ethanmuthoniw6@gmail.com>
2024-01-09 22:55:17 +0300

    update task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,13 +9,11 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def index() -> str:
     '''
     returns a title and header when / is called
     '''
-    title = 'Welcome to Holberton'
-    header = 'Hello world'
-    return render_template('0-index.html', title=title, header=header)
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
